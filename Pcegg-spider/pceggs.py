5a1a8664d8ff81c977b275711ca2f5037ef93d47
yangzc <850224169@qq.com>
2020-08-17 23:35:39 +0800

    Pceggs : Daily Update and strength the spider
    Data : 2020.08.17
    Description : Support the handle of Exception for spider.
                  Support more safe method to find element.

--- Pcegg-spider/pceggs.py
+++ Pcegg-spider/pceggs.py
@@ -2,59 +2,95 @@ import time
 from selenium import webdriver
 
 
-def login():
+def login(driver):
     acount_num = 'qq850224169'
     passwd_str = 'yzhch123456789'
-    driver = webdriver.Chrome()
-    url = 'http://www.pceggs.com/'
-    driver.get(url)
-    time.sleep(3)
     # 使用CSSSelector正则匹配头部
     #elem = driver.find_element_by_css_selector("iframe[id^='x-URS-iframe']")
     # 163登陆框是使用iframe进行嵌套的，所以需要先切换到该iframe
     #driver.switch_to.frame(elem)
 
-    acount = driver.find_element_by_name('txt_UserName')
+    acount = findObjSafely(driver,'name','txt_UserName')
     acount.clear()
     acount.send_keys(acount_num)
 
-    passwd = driver.find_element_by_name('txt_PWD')
+    passwd = findObjSafely(driver,'name','txt_PWD')
     passwd.clear()
     passwd.send_keys(passwd_str)
 
-    vercode = driver.find_element_by_name('txt_VerifyCode')
+    vercode = findObjSafely(driver,'name','txt_VerifyCode')
     vercode.clear()
-
     vercode_str = input("请输入验证码:\n")
     vercode.send_keys(vercode_str)
 
-    time.sleep(3)
-    click_button = driver.find_element_by_name('Login_Submit')
+    click_button = findObjSafely(driver,'name','Login_Submit')
     click_button.click()
     time.sleep(5)
     cur_cookies = driver.get_cookies()[0]
     return cur_cookies
 
+def findObjSafely(driver,type,str):
+    waitForObjExist(driver,type,str)
+    if(type == 'name'):
+        return driver.find_element_by_name(str)
+    elif(type == 'id'):
+        return driver.find_element_by_id(str)
+
+
 
 def isObjExist(driver, type, str):
+    from selenium.common.exceptions import NoSuchElementException
     if type == 'name':
-        obj = driver.find_element_by_name(str)
+        try:
+            obj = driver.find_element_by_name(str)
+        except NoSuchElementException:
+            return False
+        else:
+            return True
     elif type == 'id':
-        obj = driver.find_element_by_id(str)
-
-    if not obj :
-        existance = True
-    else:
-        existance = False
-    return existance
-
-
+        try:
+            obj = driver.find_element_by_id(str)
+        except NoSuchElementException:
+            return False
+        else:
+            return True
 def Test():
     driver = webdriver.Chrome()
     url = 'http://www.pceggs.com/'
     driver.get(url)
-    existance = isObjExist(driver, 'name', 'jajajajajajajjajaja')
+    existance = isObjExist(driver, 'name', 'txt_UserName')
     print(existance)
 
+def isLogin(driver):
+    tickFor1S = 0
+    while isObjExist(driver,'id','fc_loginHead') == True and tickFor1S < 5:
+        time.sleep(1)
+        tickFor1S = tickFor1S + 1
+    if isObjExist(driver,'id','fc_loginHead') == True:
+        return False
+    else:
+        return True
+
+def waitForObjExist(driver,type,str):
+    tickFor1S = 0
+    waitTime = 5
+    while isObjExist(driver,type,str) == False and tickFor1S < waitTime:
+        time.sleep(1)
+        tickFor1S = tickFor1S + 1
+    try:
+        if tickFor1S >= waitTime:
+            raise Exception(type + '不存在')
+    except:
+            handleObjExistErr()
+
+def handleObjExistErr():
+    return
+
+
 if __name__ == '__main__':
-    Test()
+    driver = webdriver.Chrome()
+    url = 'http://www.pceggs.com/'
+    driver.get(url)
+    if not isLogin(driver):
+        login(driver)
+
