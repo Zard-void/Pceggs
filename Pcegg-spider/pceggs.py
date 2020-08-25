429e5aae597370b28ea6b8e7401ef08ca3e03a4c
yangzc <850224169@qq.com>
2020-08-26 00:04:41 +0800

    Pceggs : strength the spider
    Data : 2020.08.26
    Description : support the function of getting information of the first row

--- Pcegg-spider/pceggs.py
+++ Pcegg-spider/pceggs.py
@@ -1,5 +1,6 @@
 import time
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 import json
 import xlwings as xw
 
@@ -47,6 +48,8 @@ def findObjSafely(driver,type,str):
             return driver.find_element_by_id(str)
         elif(type == 'tag_name'):
             return driver.find_element_by_tag_name(str)
+        elif(type == 'xpath'):
+            return driver.find_element_by_xpath(str)
     else:
         return None
 
@@ -70,7 +73,14 @@ def isObjExist(driver, type, str):
             return True
     elif type == 'tag_name':
         try:
-            boj = driver.find_element_by_tag_name(str)
+            obj = driver.find_element_by_tag_name(str)
+        except NoSuchElementException:
+            return False
+        else:
+            return True
+    elif type == 'xpath':
+        try:
+            obj = driver.find_element_by_xpath(str)
         except NoSuchElementException:
             return False
         else:
@@ -111,24 +121,25 @@ def loginWithCookie(driver):
     driver.delete_all_cookies()
     with open('cookies.txt', 'r', encoding='utf8') as f:
         listCookies = json.loads(f.read())
-        print(listCookies)
     for cookie in listCookies:
         driver.add_cookie(cookie)
     driver.refresh()
     return
 
 def Test():
-    wb = xw.Book("D:\\Test\\anjian.xlsm")
-    sht0 = wb.sheets[0]
-    value = sht0.range('A1').value
-    print(value)
-    value = sht0.range('A8').value
-    print(value)
+    wb = xw.Book("D:\\Test\\database.xlsm")
+    wb.sheets['sheet1'].range('A2').value='人生'
+    wb.sheets['sheet1'].api.Rows(2).Insert()
+
 
 
 if __name__ == '__main__':
-    if(0):
-        driver = webdriver.Chrome()
+    if(1):
+        listData = []
+        chrome_options = Options()
+        chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
+        chrome_driver  = "D:\\CodingTools\\Python3.7.0\\chromedriver.exe"
+        driver = webdriver.Chrome(options=chrome_options)
         url = 'http://www.pceggs.com/'
         driver.get(url)
         if not isLogin(driver):
@@ -136,12 +147,40 @@ if __name__ == '__main__':
         moveToStartPage(driver)
         table = findObjSafely(driver,'id','panel')
         trlist = table.find_elements_by_tag_name('tr')
-        print(len(trlist))
         for row in trlist:
             #遍历行对象，获取每一个行中所有的列对象
             tdlist = row.find_elements_by_tag_name('td')
-            for col in tdlist:
-                print(col.text + '\t', end='')
-            print('\n')
+            #0:期号 1:时间 2:号码 3:人数 4:总额 5:中 6:我中 7:状态
+            if (tdlist[7].text == "已揭晓"):
+                listData.append(tdlist[0].text)
+                tagContent = tdlist[2].text
+                listData.append(tagContent.split("+", 2)[0].split(" ",1)[0])
+                #print(tagContent.split("+",2)[0].split(" ",1)[0])
+                listData.append(tagContent.split("+", 2)[1].split(" ",1)[1])
+                #print(tagContent.split("+", 2)[1].split(" ",1)[1])
+                listData.append(tagContent.split("+", 2)[2].split(" ",2)[1])
+                #print(tagContent.split("+", 2)[2].split(" ",2)[1])
+                #print("已揭晓")
+                tdlist[7].click()
+                tagContent = findObjSafely(driver,'xpath','//td[contains(text(),"期号")]')
+                tagNum = tagContent.text.split("：",1)[1]
+                if (tagNum == listData[0]):
+                    tableMain = findObjSafely(driver,'xpath','//table[ @ cellspacing = "1"]')
+                    tableRowList = tableMain.find_elements_by_tag_name('tr')
+                    index = 0
+                    for childRow in tableRowList:
+                        tableColumnList = childRow.find_elements_by_tag_name('td')
+                        if(isObjExist(tableColumnList[0],'tag_name','img')):
+                            img = findObjSafely(tableColumnList[0],'tag_name','img')
+                            imgContent = img.get_attribute("src")
+                            tmpStr = "number_" + str(index) + ".gif"
+                            if tmpStr in imgContent:
+                                listData.append(tableColumnList[1].text)
+                                index = index + 1
+                else:
+                    print("OMG")
+                for obj in listData:
+                    print(obj)
+                time.sleep(30)
     else:
         Test()
